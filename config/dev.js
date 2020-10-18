c36813437fa736047366212d7d53a9da015f04ba
Kelocoes <51235539+Kelocoes@users.noreply.github.com>
2020-10-18 15:40:12 -0500

    Delete dev.js

--- config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    posgresqlURI: 'postgres://ofqrvarejipzxx:75200e07a90bee1b2ca9bf9db02621e80e548ff0e8deb8d027f40aa436679474@ec2-18-235-109-97.compute-1.amazonaws.com:5432/d6q1dbin9tu06s',
-  };
